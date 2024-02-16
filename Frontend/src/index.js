6413f8488c04b279ca60e9efe11b2b8b3c019733
Anuj Dagade <dagadeanuj@gmail.com>
2024-02-16 15:12:17 +0000

    Add submit button and submit functionality

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -6,3 +6,18 @@ addTextBox.addEventListener("click", () => {
 	addComp(++counter);
 });
 
+const submit = document.querySelector("#submit");
+submit.addEventListener("click", async () => {
+	const view = document.querySelector(".view").innerHTML;
+	
+	const res = await fetch("/api/submit", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ view }),
+	});
+	const data = await res.json();
+	
+
+});
