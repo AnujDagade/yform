fe133e99c4ed9b886872633d473eadda2a8cff45
AnujDagade <dagadeanuj@gmail.com>
2024-02-22 17:53:12 +0000

    Env setup

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,6 +1,7 @@
 import { addComp } from "./addComp";
 let counter = 0;
 
+
 const addTextBox = document.querySelector("#textBox");
 addTextBox.addEventListener("click", () => {
 	addComp(++counter);
